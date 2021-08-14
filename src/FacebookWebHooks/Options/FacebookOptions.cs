77dd45d48d06cfc00ea297b73bfdf6c6fe6b4cd1
dovanloc2466 <49464845+dovanloc2466@users.noreply.github.com>
2021-08-14 11:10:00 +0700

    init

---
+++ src/FacebookWebHooks/Options/FacebookOptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FacebookWebHooks
+{
+    public class FacebookOptions
+    {
+        public string VerifyToken { get; set; }
+        public bool ShouldVerifySignature { get; set; }
+        public string AppSecret { get; set; }
+        public string AppToken { get; set; }
+    }
+}
---
+++ src/FacebookWebHooks/Tools/Hash.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookWebHooks
+{
+    public static class Hash
+    {
+        public static string ComputeHash(string secretKey, string textToHash)
+        {
+            byte[] secret = Encoding.UTF8.GetBytes(secretKey);
+            var hasher = new HMACSHA1(secret);
+
+            byte[] textBytes = Encoding.UTF8.GetBytes(textToHash);
+
+            return ToHex(hasher.ComputeHash(textBytes));
+        }
+
+      
+        public static string ToHex(byte[] data)
+        {
+            if (data == null)
+            {
+                return string.Empty;
+            }
+
+            char[] content = new char[data.Length * 2];
+            int output = 0;
+            byte d;
+            for (int input = 0; input < data.Length; input++)
+            {
+                d = data[input];
+                content[output++] = HexLookup[d / 0x10];
+                content[output++] = HexLookup[d % 0x10];
+            }
+            return new string(content);
+        }
+
+        private static readonly char[] HexLookup = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+    }
+}
